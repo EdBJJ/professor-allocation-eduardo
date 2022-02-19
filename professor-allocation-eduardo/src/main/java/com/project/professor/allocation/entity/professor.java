a9bc82801a672df4cc60f74f80b5470ee98b0c66
EdBJJ <faceccjj@gmail.com>
2022-02-18 22:49:58 -0300

    adicionando camada de modelo

---
+++ professor-allocation-eduardo/src/main/java/com/project/professor/allocation/entity/professor.java
@@ -0,0 +1,60 @@
+package com.project.professor.allocation.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@Column(length = 14, unique = true, nullable = false)
+	private String cpf;
+
+	@Column(name = "departmentId", nullable = false)
+	private Long departmentId;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "departmentId", updatable = false, insertable = false)
+	private department department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+}
